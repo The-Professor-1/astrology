29a20de3e665bcfbf22db77f94d8c3998b120a0a
The-Professor-1 <maymerlibe@gmail.com>
2025-03-14 22:20:16 +0300

    new hosts added

--- astrology/settings.py
+++ astrology/settings.py
@@ -4,9 +4,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-ko!nbxq^_s2&)o^4w!ya+t_)9u_25zcmdh)u3@hq26knoz$p*d')
-#DEBUG = os.getenv('DEBUG', 'True') == 'True'
-DEBUG = True
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,sinekewakibt.vercel.app').split(',')
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,sinekewakibt-git-main-the-professor-1s-projects.vercel.app,sinekewakibt-ozj7o11tf-the-professor-1s-projects.vercel.app,sinekewakibt.vercel.app').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
