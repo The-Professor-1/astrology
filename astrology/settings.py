b4c85a0e962bf5278b6499a70e61f22d470f3d77
The-Professor-1 <maymerlibe@gmail.com>
2025-03-14 20:56:22 +0300

    set debug true temporarly

--- astrology/settings.py
+++ astrology/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-ko!nbxq^_s2&)o^4w!ya+t_)9u_25zcmdh)u3@hq26knoz$p*d')
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,sinekewakibt.vercel.app').split(',')
 
 INSTALLED_APPS = [
